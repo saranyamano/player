983bf0b6b448b0aaf53a446f874739155c9e1988
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-03-19 16:24:12 +0530

    Update p24.c

--- p24.c
+++ p24.c
@@ -7,17 +7,17 @@ printf("ENTER THE STRING:");
 scanf("%s",&s);
 for(i=0;s[i]!='\0';i++)
 {
-if((s[i]>=0)&&(s[i]<=9))
+if((s[i]>='0')&&(s[i]<='9'))
 {
 f=1;
 }
 }
 if(f==0)
 {
-  printf("Yes");
+  printf("No");
 }
 else
 {
-printf("No");
+printf("Yes");
 }
 }
